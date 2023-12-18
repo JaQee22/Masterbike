736c16d4d46c2b27beb48b82e070fa59872e4660
Jacqueline Morales <114114677+JaQee22@users.noreply.github.com>
2023-12-18 19:29:26 -0300

    .

--- myfirebaseexample/app/src/main/java/com/example/myfirebaseexample/api/response/BiciResponse.kt
+++ myfirebaseexample/app/src/main/java/com/example/myfirebaseexample/api/response/BiciResponse.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class BiciResponse(
     @SerializedName("00_Id") var id: String,
-    @SerializedName("01_Lugar") var name: String,
+    @SerializedName("01_Servicio") var name: String,
     @SerializedName("14_Cliente") var names: String,
     @SerializedName("16_Costo") var cost: Long,
     @SerializedName("20_Servicio") var brand: String
